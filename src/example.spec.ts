94abae845d48e3c9314edf6f7b31c7e940e7e542
Gabriel Lebec <glebec@gmail.com>
2019-06-20 02:06:55 -0400

    feat: working fast-check example

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,7 +1,16 @@
-import greeter from './example'
+import * as fc from 'fast-check'
+import greeter, { contains } from './example'
 
-describe('greeter', () => {
+describe('`greeter`', () => {
     it('greets', () => {
         expect(greeter('you')).toBe('Hello, you')
+    })
+})
+
+describe('`contains`', () => {
+    it('checks containment', () => {
+        fc.assert(fc.property(fc.string(), fc.string(), (s1, s2) => {
+            return contains(s2, s1 + s2)
+        }))
     })
 })
--- src/example.ts
+++ src/example.ts
@@ -2,6 +2,7 @@ export default function greeter (person: string): string {
     return 'Hello, ' + person
 }
 
-const user : string = 'Jane User'
-
-console.log(greeter(user))
+export function contains (pattern: string, text: string): boolean {
+    // return text.substr(1).indexOf(pattern) !== -1 // fails
+    return  text.indexOf(pattern) !== -1 // works
+}
