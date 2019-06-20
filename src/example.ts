ca588340bd8795cd239dd15184b059f9d300a6fb
Gabriel Lebec <glebec@gmail.com>
2019-06-20 01:40:06 -0400

    chore: object types -> primitive types

--- src/example.ts
+++ src/example.ts
@@ -1,7 +1,7 @@
-export default function greeter (person: String): String {
+export default function greeter (person: string): string {
     return 'Hello, ' + person
 }
 
-const user : String = 'Jane User'
+const user : string = 'Jane User'
 
 console.log(greeter(user))
